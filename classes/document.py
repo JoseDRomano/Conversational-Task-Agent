d9407b3076881fcec007085fa05ba4f967e1e956
Jaime <ja.russo@campus.fct.unl.pt>
2024-04-03 12:47:58 +0100

    +Test notebook +A small class to deal with documents

---
+++ classes/document.py
@@ -0,0 +1,33 @@
+import json
+
+with open("./recipes/recipes_data.json", "r") as read_file:
+    data = json.load(read_file)
+
+ingredients = data['12']['ingredients']
+
+for ingredient in ingredients:
+    print(ingredient['displayText'])
+ 
+def index_document(client, index_name):
+    count = 0
+    for key, recipe in data.items():
+        count += 1
+        ingredients = recipe[ingredients]
+        
+        doc = {
+            'title': recipe['displayName'],
+            'description': recipe['description'],
+            'ingredients': recipe['ingredients'],     #TODO
+            'duration': recipe['totalTimeMinutes'],
+            'steps': recipe['instructions']           #TODO   
+        }
+        resp = client.index(index=index_name, id=count, body=doc)
+        print(resp['result'])
+
+#add this method to the method above when romano is done
+def get_ingredients(recipe):
+    ingredients = recipe[ingredients]
+
+    for ingredient in ingredients:
+        print(ingredient['displayText'])
+    return
