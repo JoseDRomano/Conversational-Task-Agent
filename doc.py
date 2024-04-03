954a8c85bd821ca1feb9e84221e070bbe55a0e19
Jaime <ja.russo@campus.fct.unl.pt>
2024-04-04 00:57:50 +0100

    project notebook added, also doc.py (class dedicated to the document creation)

--- doc.py
+++ doc.py
@@ -1,33 +1,31 @@
 import json
 
-with open("./recipes/recipes_data.json", "r") as read_file:
-    data = json.load(read_file)
-
-ingredients = data['12']['ingredients']
-
-for ingredient in ingredients:
-    print(ingredient['displayText'])
- 
-def index_document(client, index_name):
+def index_document(client, index_name, data):
     count = 0
     for key, recipe in data.items():
         count += 1
-        ingredients = recipe[ingredients]
-        
+
+        title = recipe['displayName']
+
         doc = {
-            'title': recipe['displayName'],
+            'title': title,
             'description': recipe['description'],
-            'ingredients': recipe['ingredients'],     #TODO
+            'ingredients': get_ingredients(recipe),
             'duration': recipe['totalTimeMinutes'],
-            'steps': recipe['instructions']           #TODO   
+            'steps': get_steps(recipe)
         }
         resp = client.index(index=index_name, id=count, body=doc)
-        print(resp['result'])
+        print(resp['result'] + ": " + title)
 
-#add this method to the method above when romano is done
+
 def get_ingredients(recipe):
-    ingredients = recipe[ingredients]
-    
-    for ingredient in ingredients:
-        print(ingredient['displayText'])
-    return
+    ingredients = []
+    for ingredient in recipe['ingredients']:
+        ingredients.append(ingredient['ingredient'])
+    return ingredients
+
+def get_steps(recipe):
+    steps = []
+    for step in recipe['instructions']:
+        steps.append(step['stepText'])
+    return steps
